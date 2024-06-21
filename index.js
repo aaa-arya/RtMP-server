72e11a5ec649851fb6bed3abe01b6556a70d1079
aaa-arya <aryasujeet680@gmail.com>
2024-06-21 22:46:44 +0530

    h

--- index.js
+++ index.js
@@ -7,11 +7,16 @@ const config = {
       gop_cache: true,
       ping: 60,
       ping_timeout: 30
-  },
-  http: {
-      port: 8000,
-      allow_origin: '*'
   }
+  
+  // ,
+  // http: {
+  //     port: 8000,
+  //     allow_origin: '*'
+    
+
+  // }
+
 };
 var nms = new NodeMediaServer(config);
   nms.run();
