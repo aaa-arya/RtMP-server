2c35d47838b0d7080b8616c08f5e6ca579737062
aaa-arya <aryasujeet680@gmail.com>
2024-06-21 23:00:26 +0530

    sd

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const config = {
 var nms = new NodeMediaServer(config);
   nms.run();
 
-//  nms.stop();
+ nms.stop();
 console.log("okkdone half");
 nms.on('getFilePath', (streamPath, oupath, mp4Filename) => {
   console.log('---------------- get file path ---------------');
